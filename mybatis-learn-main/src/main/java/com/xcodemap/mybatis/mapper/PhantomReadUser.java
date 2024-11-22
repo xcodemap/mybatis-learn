df4ee0261ce9590e48b2b27da9d9474b171a88b6
xcodemap <xcodemap@gmail.com>
2024-11-22 12:50:49 +0800

    Add phantom read example

---
+++ mybatis-learn-main/src/main/java/com/xcodemap/mybatis/mapper/PhantomReadUser.java
@@ -0,0 +1,51 @@
+package com.xcodemap.mybatis.mapper;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class PhantomReadUser {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PhantomReadUser.class);
+    private static void doSomeThing(UserMapper mapper) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("age", 101);
+        List<User> users = mapper.findUser(params);
+        handleUsers(users);
+    }
+
+
+    private static void handleUsers(List<User> users) {
+        boolean hasAdmin = false;
+        for (User user : users) {
+            if (user.getName().equals("admin")) {
+                hasAdmin = true;
+            }
+        }
+        if (!hasAdmin) {
+            users.add(new User(0, "admin", 12));
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        String resource = "mappers/mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        try (SqlSession session = sqlSessionFactory.openSession()) {
+            UserMapper mapper = session.getMapper(UserMapper.class);
+            doSomeThing(mapper);
+            Map<String, Object> params = new HashMap<>();
+            params.put("age", 101);
+            List<User> users = mapper.findUser(params);
+            System.out.println(users);
+        }
+    }
+}
--- mybatis-learn-main/src/main/java/com/xcodemap/mybatis/mapper/User.java
+++ mybatis-learn-main/src/main/java/com/xcodemap/mybatis/mapper/User.java
@@ -5,6 +5,15 @@ public class User {
     private String name;
     private int age;
 
+    public User() {
+    }
+
+    public User(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
     public int getId() {
         return id;
     }
